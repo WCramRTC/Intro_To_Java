d0896b71b75a5ea2ab8c7abf6f013904dd835fea
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-10-16 19:41:44 -0700

    Initial commit

---
+++ src/main/java/com/wcram/intro_to_java/Intro_To_Java.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.wcram.intro_to_java;
+
+/**
+ *
+ * @author WCram
+ */
+public class Intro_To_Java {
+    
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        // System.out.println() is the same as Console.WriteLine()
+    }
+    
+    // Ctrl + / is the comment AND uncomment hotkey
+   
+    
+}
